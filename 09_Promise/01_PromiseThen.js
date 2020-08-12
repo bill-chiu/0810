233a0cc5a536e08f5315b0d12419af3709eeaa28
CS01183 <68837493+bill-chiu@users.noreply.github.com>
2020-08-12 11:07:42 +0800

    0812 promise

--- 09_Promise/01_PromiseThen.js
+++ 09_Promise/01_PromiseThen.js
@@ -1,17 +1,19 @@
-function longTimeWork(workFine = true, errorMessage = "test") {
+function longTimeWork(workFine = true, errorMessage = "test45645") {
     return new Promise( (resolve, reject) => {
         setTimeout( () => {
-            (workFine) ? resolve(200) : reject(errorMessage);
+            (workFine) ? resolve(201) : reject(errorMessage);
         }, 1000);
     })
 }
 
 function usingLongTimeWork() {
-    longTimeWork(true, "test")  // try true/false
+    longTimeWork(false, "test111")  // try true/false
     .then(function (e) {
+        console.log('1');
         console.log(e);
     })
     .catch(function (e) {
+        console.log('2');
         console.log(e);
     })
 }
--- 09_Promise/04_StillStepByStep.js
+++ 09_Promise/04_StillStepByStep.js
@@ -17,10 +17,18 @@ function sad(data, timeCount) {
 
 async function living() {
     var total = 0;
-    var result1 = await happy(200, 2000);
-    console.log(result1);
-    var result2 = await sad(-100, 3000);
-    console.log(result2);
+    var Promise1 = happy(200, 2000);
+    // var result1=await Promise1;
+
+    var Promise2 = sad(-100, 3000);
+
+    let [result1,result2]=
+    await Promise.all([Promise1,Promise2]);
+    // var result2 = await sad(-100, 3000);
+    // console.log(result1);
+    // console.log(result2);
+
+
     total = result1 + result2;
     console.log("total:", total);
 }
